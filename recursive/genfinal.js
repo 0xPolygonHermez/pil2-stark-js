cab69a0a924fc8c25b1a0a8c0c6ba9433e54fce4
RogerTaule <roger.taulee@gmail.com>
2023-11-28 11:57:57 +0100

    Adding global constraints to final circuit

--- recursive/genfinal.js
+++ recursive/genfinal.js
@@ -3,7 +3,7 @@ const path = require("path");
 const ejs = require("ejs");
 const { Transcript } = require('./templates/transcript');
 
-module.exports.genFinal = async function genFinal(globalInfo, starkInfoRecursives2) {
+module.exports.genFinal = async function genFinal(globalInfo, globalConstraints, starkInfoRecursives2) {
 
     if(!globalInfo) throw new Error("Global info is undefined");
     if(!globalInfo.nPublics) throw new Error("Global info does not contain number of publics");
@@ -24,6 +24,7 @@ module.exports.genFinal = async function genFinal(globalInfo, starkInfoRecursive
     const template = await fs.promises.readFile(path.join(__dirname, "templates", `final.circom.ejs`), "utf8");
 
     const obj = {
+        globalConstraints,
         starkInfoRecursives2,
         nPublics,
         nChallengesStages,
