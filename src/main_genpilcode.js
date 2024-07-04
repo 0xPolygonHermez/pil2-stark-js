4d1f3be0b69bf2da6e4e1a75f58a8194b3a8fbe0
RogerTaule <roger.taulee@gmail.com>
2024-07-04 10:45:41 +0200

    Updating py files

--- src/main_genpilcode.js
+++ src/main_genpilcode.js
@@ -65,13 +65,13 @@ async function run() {
         for(let i = 1; i <= res.nStages + 1; ++i) {
             let stage = i;
             let stageName = "cm" + stage;
-            let nColsStage = res.cmPolsMap.filter(p => p.stage == stageName).length;
+            let nColsStage = res.cmPolsMap.filter(p => p.stage == stage).length;
             nCols[stageName] = nColsStage;
             let nColsBaseField = res.mapSectionsN[stageName];
             if(i === res.nStages + 1 || (i < res.nStages && !res.imPolsStages)) {
                 console.log(`Columns stage ${stage}: ${nColsStage} -> Columns in the basefield: ${nColsBaseField}`);
             } else {
-                console.log(`Columns stage ${stage}: ${nColsStage} (${res.cmPolsMap.filter(p => p.stage == stageName && p.imPol).length} intermediate columns) -> Columns in the basefield: ${nColsBaseField}`);
+                console.log(`Columns stage ${stage}: ${nColsStage} (${res.cmPolsMap.filter(p => p.stage == stage && p.imPol).length} intermediate columns) -> Columns in the basefield: ${nColsBaseField}`);
             }
             nColumns += nColsStage;
             nColumnsBaseField += nColsBaseField;
